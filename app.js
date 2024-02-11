8ba4a1e7dc3d73e85305c2baaf99d10437ce7f3f
voskreseniev <roman.mayber@gmail.com>
2024-02-11 13:14:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -93,7 +93,6 @@ function logout() {
   }
 }
 
-
 document.addEventListener('click', function() {
   if (firebase.auth().currentUser) {
     incrementScore();
@@ -108,6 +107,24 @@ function incrementScore() {
   }
 }
 
+// Переменные для отслеживания скорости кликов
+let lastClickTime = 0;
+let clickCount = 0;
+
+document.addEventListener('click', function() {
+  const currentTime = new Date().getTime();
+  const timeDiff = currentTime - lastClickTime;
+
+  if (timeDiff < 300) {
+    clickCount++;
+  } else {
+    clickCount = 1;
+  }
+
+  lastClickTime = currentTime;
+
+  incrementScore(clickCount);
+});
 
 function loadUserData() {
   const userId = firebase.auth().currentUser.uid;
@@ -151,7 +168,6 @@ function saveScore(score) {
   }
 }
 
-
 function suggestUsername() {
   const defaultUsername = "User"; // Значение по умолчанию
   let displayName = prompt("Пожалуйста, введите ваше имя:", defaultUsername);
@@ -190,8 +206,6 @@ function updateUpgradeCost(upgradeIndex) {
 function startAutoclick() {
   autoclickInterval = setInterval(incrementScore, 1000);
 }
-
-let lastClickTime = 0;
 
 usernameDisplay.addEventListener('click', function() {
   const currentTime = new Date().getTime();
